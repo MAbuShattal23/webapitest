e60738551e92be933b3a5cd2d2f3fc42f9e5adc7
MAbuShattal23 <abushattal@ieee.org>
2024-03-18 05:27:44 -0400

    First Webtestapi: Add Controllers, model Employee Class, connect to SQlite, run with Postman, and db migrations.

---
+++ Migrations/20240318091859_Initial.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapicontrollers.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fname = table.Column<string>(type: "TEXT", nullable: true),
+                    Minit = table.Column<string>(type: "TEXT", nullable: true),
+                    Lname = table.Column<int>(type: "INTEGER", nullable: false),
+                    Ssn = table.Column<int>(type: "INTEGER", nullable: false),
+                    Bdate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Address = table.Column<string>(type: "TEXT", nullable: true),
+                    Sex = table.Column<char>(type: "TEXT", nullable: false),
+                    Salary = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,11 @@
+public class Employee{
+    public int Id {get; set;}
+    public string? Fname { get; set; } 
+    public string? Minit { get; set; }
+    public int Lname { get; set; }
+    public int Ssn { get; set; }
+    public DateTime Bdate { get; set; }
+    public string? Address { get; set; }    
+    public char Sex { get; set; }
+    public double Salary { get; set; }
+}
